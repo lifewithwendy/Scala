4e4c6e07ca97990912398fe75b31ed14c4621854
lifewithwendy <2022cs048@stu.ucsc.cmb.ac.lk>
2024-07-02 05:45:00 +0530

    correction 1

--- practicle3/1/prac_1.scala
+++ practicle3/1/prac_1.scala
@@ -1,12 +1,14 @@
 // Write a function in Scala that takes a string as input and returns the reverse of the string
 // using a recursive approach.
-
+import scala.io.StdIn
 object prac_1{
     def reverseString(input: String): String = {
         if (input.isEmpty) {
             ""
         }else {
             reverseString(input.tail) + input.head
+            // .tail - the rest of the string without the 1st character
+            // .head - 1st char
         }
     }
 
---
+++ practicle3/2/prac_2.scala
@@ -0,0 +1,15 @@
+// Write a function in Scala that takes a list of strings as input and returns a new list
+// containing only the strings that have a length greater than 5.
+
+object prac_2 {
+def filterStrings(strings: List[String]): List[String] = {
+    strings.filter(_.length > 5)
+    // s => s.length > 5
+}
+
+def main(args: Array[String]): Unit = {
+    val inputStrings = List("apple", "banana", "cherry", "date", "elderberry", "fig", "grape")
+    val filteredStrings = filterStrings(inputStrings)
+    println("Filtered strings: " + filteredStrings)
+}
+}
